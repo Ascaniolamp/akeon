98a7b2813826acc350a42e5b90f0ff2172afceb0
Ascaniolamp <ascaniolamp@gmail.com>
2024-02-20 19:24:05 +0100

    Added death if into itself and put body variable into struct, dividing it into ybody and xbody arrays

--- snake.c
+++ snake.c
@@ -1,5 +1,6 @@
 #include <ncurses.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
 #include <time.h>
@@ -8,6 +9,14 @@ int randrange(int min, int max){ return random() % (max + 1 - min) + min; }
 
 void endprogram(int code){
 	endwin();
+
+	switch(code){
+		case 0: printf("You won!\n"); break;
+		case 1: printf("You lost...\n"); break;
+		case 2: exit(0);
+		default: printf("ERROR (Exit code %d)\n",code); break;
+	}
+
 	exit(code);
 }
 
@@ -32,19 +41,20 @@ void initprogram(){
 
 void borders_render();
 void apple_regen(int *ypos, int *xpos);
-void snake_movement(int *ypos, int *xpos);
+void snake_movement(int *ypos, int *xpos, int *ydir, int *xdir);
 
 int main(int argc, char const *argv[]){
 	initprogram();
 
 	const float DIFFICULTY = 0.95;
 	const int AREA = (COLS-2)*(LINES-2);
-	const int MAXLEN = AREA/2;
 	int delay = 200000;
 
 	typedef struct{
 		char head_symbol, body_symbol;
-		int ypos, xpos, size;
+		int ybody[AREA], xbody[AREA];
+		int ydir, xdir;
+		int size;
 	}Snake;
 
 	typedef struct{
@@ -52,49 +62,56 @@ int main(int argc, char const *argv[]){
 		int ypos, xpos;
 	}Apple;
 
-	Snake snake = {'@', 'x', LINES/2, COLS/2, 3};
-	Apple apple = {'O', 0, 0};
+	Snake snake;
+	snake.head_symbol = '@';
+	snake.body_symbol = 'x';
+	snake.ybody[0] = LINES/2;
+	snake.xbody[0] = COLS/2;
+	snake.ydir = 0;
+	snake.xdir = 0;
+	snake.size = 3;
 
-	int body[2][MAXLEN];
-	for(int i=0; i<snake.size; i++){
-		body[0][i] = snake.ypos;
-		body[1][i] = snake.xpos;
-	}
+	Apple apple = {'O', 0, 0};
 
 	apple_regen(&apple.ypos, &apple.xpos);
 
 	while(true){
 		erase();
 
-		snake_movement(&snake.ypos, &snake.xpos);
-
 		// BODY MOVEMENT
-		for(int i=snake.size+1; i>0; i--){
-			body[0][i] = body[0][i-1];
-			body[1][i] = body[1][i-1];
+		for(int i=snake.size-1; i>0; i--){
+			snake.ybody[i] = snake.ybody[i-1];
+			snake.xbody[i] = snake.xbody[i-1];
 		}
-		body[0][0] = snake.ypos;
-		body[1][0] = snake.xpos;
+
+		snake_movement(&snake.ybody[0], &snake.xbody[0], &snake.ydir, &snake.xdir);
 
 		// SNAKE DEATH
-		if(snake.ypos > LINES-2 || snake.ypos < 1) endprogram(1);
-		if(snake.xpos > COLS-2 || snake.xpos < 1) endprogram(1);
+		if(snake.ybody[0] > LINES-2 || snake.ybody[0] < 1) endprogram(1);
+		if(snake.xbody[0] > COLS-2 || snake.xbody[0] < 1) endprogram(1);
+		for(int i=1; i<snake.size; i++){
+			bool in_body = (snake.ybody[0] == snake.ybody[i]) && (snake.xbody[0] == snake.xbody[i]);
+			bool is_moving = (snake.ydir != 0) || (snake.xdir !=0);
+			if(in_body && is_moving){
+				endprogram(1);
+			}
+		}
 
 		// APPLE EAT
-		if(snake.ypos == apple.ypos && snake.xpos == apple.xpos){
-			if(snake.size++ >= MAXLEN) endprogram(0);
+		if(snake.ybody[0] == apple.ypos && snake.xbody[0] == apple.xpos){
+			if(snake.size++ >= AREA) endprogram(0);
 			apple_regen(&apple.ypos, &apple.xpos);
 			delay *= DIFFICULTY;
 		}
 
+		// RENDER SNAKE
+		attrset(COLOR_PAIR(1));
+		for(int i=1; i<snake.size; i++) mvaddch(snake.ybody[i], snake.xbody[i], snake.body_symbol);
+		mvaddch(snake.ybody[0], snake.xbody[0], snake.head_symbol);
+
 		// RENDER APPLE
 		attrset(COLOR_PAIR(2));
 		mvaddch(apple.ypos, apple.xpos, apple.symbol);
-
-		// RENDER SNAKE & BODY
-		attrset(COLOR_PAIR(1));
-		mvaddch(snake.ypos, snake.xpos, snake.head_symbol);
-		for(int i=1; i<snake.size; i++) mvaddch(body[0][i], body[1][i], snake.body_symbol);
 
 		// RENDER BORDERS
 		borders_render();
@@ -125,28 +142,14 @@ void apple_regen(int *ypos, int *xpos){
 	*xpos = randrange(1,COLS-2);
 }
 
-void snake_movement(int *ypos, int *xpos){
-	static int ydir, xdir;
-
+void snake_movement(int *ypos, int *xpos, int *ydir, int *xdir){
 	switch(getch()){
-		case KEY_UP:
-			ydir = -1;
-			xdir = 0;
-			break;
-		case KEY_DOWN:
-			ydir = 1;
-			xdir = 0;
-			break;
-		case KEY_LEFT:
-			ydir = 0;
-			xdir = -1;
-			break;
-		case KEY_RIGHT:
-			ydir = 0;
-			xdir = 1;
-			break;
+		case KEY_UP: *ydir = -1; *xdir = 0; break;
+		case KEY_DOWN: *ydir = 1; *xdir = 0; break;
+		case KEY_LEFT: *ydir = 0; *xdir = -1; break;
+		case KEY_RIGHT: *ydir = 0; *xdir = 1; break;
 	}
 
-	*ypos += ydir;
-	*xpos += xdir;
+	*ypos += *ydir;
+	*xpos += *xdir;
 }
