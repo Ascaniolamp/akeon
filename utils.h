ddb9ec7246d74eac20f2a201e1461c6e3dc65539
Ascaniolamp <ascaniolamp@gmail.com>
2024-02-22 21:46:03 +0100

    Moved functions into header files 'utils' and 'game'

---
+++ utils.h
@@ -0,0 +1,34 @@
+void endprogram(int code);
+void initprogram();
+
+void endprogram(int code){
+	endwin();
+
+	switch(code){
+		case 0: printf("You won!\n"); break;
+		case 1: printf("You lost...\n"); break;
+		case 2: exit(0);
+		default: printf("ERROR (Exit code %d)\n",code); break;
+	}
+
+	exit(code);
+}
+
+void initprogram(){
+	signal(SIGINT, endprogram);
+	srandom(time(NULL));
+
+	initscr();
+
+	keypad(stdscr, true);
+	nodelay(stdscr, true);
+	curs_set(0);
+	nonl();
+	cbreak();
+	echo();
+
+	if(!has_colors()) return;
+	start_color();
+	init_pair(1, COLOR_RED, COLOR_BLACK); // Apple
+	init_pair(2, COLOR_WHITE, COLOR_BLUE); // Snake
+}
