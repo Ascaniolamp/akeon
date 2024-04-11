2f1d36f267f7660cb21b11492dc06240438c3a79
Ascaniolamp <ascaniolamp@gmail.com>
2024-04-11 17:17:27 +0200

    Changed order of key controls

--- game.h
+++ game.h
@@ -74,8 +74,8 @@ void snake_movement(Snake *snake){
 
 	switch(getch()){
 		case 'k': case 'w': case KEY_UP: snake->ydir = -1; snake->xdir = 0; break;
-		case 'j': case 's': case KEY_DOWN: snake->ydir = 1; snake->xdir = 0; break;
 		case 'h': case 'a': case KEY_LEFT: snake->ydir = 0; snake->xdir = -1; break;
+		case 'j': case 's': case KEY_DOWN: snake->ydir = 1; snake->xdir = 0; break;
 		case 'l': case 'd': case KEY_RIGHT: snake->ydir = 0; snake->xdir = 1; break;
 	}
 
